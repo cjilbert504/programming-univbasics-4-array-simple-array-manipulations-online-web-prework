2905746540ba022a3437976a6131ae445c0b092f
Collin Jilbert <cjilbert504@gmail.com>
2019-09-28 01:06:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,6 +11,5 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
   end
 
 def using_pop(continents)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   last_element = continents.pop
 end
