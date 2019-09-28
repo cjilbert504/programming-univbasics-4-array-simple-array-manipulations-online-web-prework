a965148edac350a4666c4192f6341bb0c5f43d24
Collin Jilbert <cjilbert504@gmail.com>
2019-09-28 00:34:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,5 +7,5 @@ end
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   new_neighborhood = "Staten Island"
-  bouroughs_in_nyc.unshift(@new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
   end
